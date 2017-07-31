e76a99d2d2c50cf71057b7e6af2439a60068ead3
mugithi <isackaranja@gmail.com>
2017-07-30 23:15:39 -0700

    changed jenkinsfile.json values: added failover policy to route53

--- route53.tf
+++ route53.tf
@@ -6,10 +6,17 @@ resource "aws_route53_record" "chart_elb_dns" {
   name    = "${var.zone_name}"
   type    = "A"
 
+  set_identifier = "set-a-primary"
+  failover_routing_policy {
+    type = "PRIMARY"
+  }
+
+
+
   alias {
     name                   = "${var.elb_name}"
     zone_id                = "${var.elb_zone_id}"
-    evaluate_target_health = "false"
+    evaluate_target_health = "true"
   }
 }
 output "route53_zone" {
